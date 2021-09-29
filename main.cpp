c8f5e405094dda798a53300c2e0de4af8ed29ccd
Cheng <765128115@qq.com>
2021-09-29 17:11:37 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 using namespace std;
 
-int fat(int x){
-    if(x==1)
+int compareWeight(int a,int b,int c){
+    if(a>b&&a>c)
     return 1;
-    else
-    return x*fat(x-1);
-
+    if(a<b&&b>c)
+    return 2;
+    if(a<c&&b<c)
+    return 3;
+    if(a==b&&b>c)
+    return 4;
+    if(a==c&&a>b)
+    return 5;
+    if(b==c&&b>a)
+    return 6;
+    if(a==b&&b==c)
+    return 7;
+    return 0;
 }
 
 int main(){
-    int result=fat(5);
-    cout<<result<<endl;
-
+   int res=rand()%99+2;
+   int guss;
+   cin>>guss;
+   while(guss!=res){
+       if(guss>res)
+       cout<<"worng & what you guess is bigger!"<<endl;
+       else
+       cout<<"worng & what you guess is smaller!"<<endl;
+       cin>>guss;
+   }
+   cout<<"right!"<<endl;
 }
